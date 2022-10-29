ade88f0a210a9e473f5bfa73eac3f5b931682a58
SammyLeths <s.o.eyiowuawi@gmail.com>
2022-10-29 09:35:19 +0100

    basic number prediction

---
+++ predict.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // No input
+  if (!guess) {
+    displayMessage('Enter a Number!');
+
+    // Player wins
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+
+    document.querySelector('body').style.backgroundColor = '#266e10';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // Wrong prediction
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Start predicting...');
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#737376';
+  document.querySelector('.number').style.width = '15rem';
+});
